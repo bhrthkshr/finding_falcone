def182fa7d879f167e9cf4bb7b20c6c617dc23eb
bhrthkshr <aninjahattori@gmail.com>
2019-07-03 18:01:57 +0530

    minor changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,6 +25,26 @@ app.controller("main.controller", function($scope, $http) {
 });
 
 app.controller("planet.controller", function($scope, $http, $rootScope) {
+
+  $scope.local_planets = [{
+    name: 'Donlon',
+    distance: '100',
+  }, {
+    name: 'Enchai',
+    distance: '200',
+  }, {
+    name: 'Jebing',
+    distance: '300',
+  }, {
+    name: 'Sapir',
+    distance: '400',
+  }, {
+    name: 'Lerbin',
+    distance: '500',
+  }, {
+    name: 'Pingasor',
+    distance: '600',
+  }]
   console.log('plannnnnnnnnneeeee');
   $scope.loaded = false;
   $scope.planets = [];
@@ -41,6 +61,15 @@ app.controller("planet.controller", function($scope, $http, $rootScope) {
   }, function errorCallback(response) {
     document.write('<h1 style="text-align:center">Something was not Found Error:404!!</h1>');
   });
+
+  //-----temp with out internet
+  // $scope.data = $scope.local_planets;
+  // $scope.loaded = true;
+  // for (var i = 0; i < $scope.data.length; i++) {
+  //   $scope.planets.push($scope.data[i]);
+  //   $scope.planets[i].selected = false;
+  // }
+  //-----------
   console.log($scope.planets, '------------');
 
   $rootScope.selected_planets = [];
@@ -99,24 +128,72 @@ app.controller("vehical.controller", function($scope, $http) {
     $scope.loaded = true;
     console.log('planets got cleared or was not loaded');
   }
+  $scope.check_existence = function(value) {
+    //to check if number of vehical is 0 or 1
+    //to deal with vehicals more than 1
+    var pos = $scope.vehicals.map(function(e) {
+      return e.name;
+    }).indexOf(value.name);
+    console.log(pos, '----is the index value of vehical');
+    return pos;
+  }
+  $scope.check_for_same_vehical = function(i, value) {
+    //this checks for a vehical is already added to a planet
+    console.log($scope.selected_planet[i].vehical, '------checking vehiii');
+    var pos = $scope.selected_planet[i].vehical.map(function(e) {
+      return e.name;
+    }).indexOf(value.name);
+    return pos;
+  }
 
   $scope.onDropComplete = function(data, evt, newState) {
     console.log("drop complete");
-    if (data.total_no != 0) {
+    var same_data = $scope.check_for_same_vehical(newState, data);
+    if (same_data != -1) {
+      console.log('sameeeeeeeeeeeee', data.name, "----", same_data);
+    } else if (same_data == -1 && $scope.selected_planet[newState].vehical.length == 0) {
+      console.log('issss diffreent');
+      // console.log($scope.selected_planet[newState].vehical[0].name == 'undefined', '----in the planet ---old');
+      if (data.total_no != 0) {
+        console.log(data.name, '----added now -- ---new');
+        $scope.check_distance(data, newState);
+      }
+      if (data.total_no == 0) {
+        var pos = $scope.check_existence(data);
+        $scope.vehicals.splice(pos, 1);
+      }
+    } else if (same_data == -1 && $scope.selected_planet[newState].vehical.length == 1) {
+      // console.log('old data is there', $scope.selected_planet[newState].vehical[0].name);
+      var temp = $scope.selected_planet[newState].vehical[0];
+      console.log(temp);
+      // if (data.total_no != 0) {
       $scope.check_distance(data, newState);
-    }
-    if (data.total_no == 0) {
-      var pos = $scope.vehicals.map(function(e) {
-        return e.name;
-      }).indexOf(data.name);
-      $scope.vehicals.splice(pos, 1);
+      var dec = $scope.check_existence(data);
+      // $scope.vehicals[dec].total_no -=1;
+      // console.log('----decreased the total number count by 1 in vehicals array');
+      // console.log($scope.selected_planet[newState].vehical);
+      var is_existing = $scope.check_existence(temp);
+      console.log(is_existing);
+      $scope.selected_planet[newState].vehical[0] = data;
+      if (is_existing != -1) {
+        $scope.vehicals[is_existing].total_no += 1;
+      } else if (is_existing == -1) {
+        temp.total_no=1;
+        $scope.vehicals.push(temp)
+      }
+      // }
+      if (data.total_no == 0) {
+        console.log('----pushed back to vehicals arry');
+        var pos = $scope.check_existence(data);
+        $scope.vehicals.splice(pos, 1);
+      }
     }
   }
 
   $scope.check_distance = function(veh, idx) {
     // console.log("came to check distance of ", veh.name, 'in planet', idx);
     if (veh.max_distance >= $scope.selected_planet[idx].distance) {
-      $scope.selected_planet[idx].vehical=[veh];
+      $scope.selected_planet[idx].vehical = [veh];
       return veh.total_no -= 1
     }
     // else {
@@ -124,11 +201,15 @@ app.controller("vehical.controller", function($scope, $http) {
     // }
   }
 
-  $scope.undo_vehical = function(vInfo,idx) {
-    console.log(vInfo,'------came back after removing');
-    $scope.vehicals.push(vInfo);
-    $scope.selected_planet[idx].vehical.splice(0,1);
-    console.log($scope.selected_planet[idx]);
+  $scope.undo_vehical = function(vInfo, idx) {
+    $scope.selected_planet[idx].vehical.splice(0, 1);
+    var isthere = $scope.check_existence(vInfo)
+    if (isthere == -1) {
+      vInfo.total_no += 1;
+      $scope.vehicals.push(vInfo);
+    } else if (isthere != -1) {
+      vInfo.total_no += 1;
+    }
   }
 
 
